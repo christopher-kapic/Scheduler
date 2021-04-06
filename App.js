97c27740450ab8c1327bbcd264f965cfa38f08b4
Christopher Kapic <christopherkapic@gmail.com>
2021-04-05 22:39:36 -0500

    First commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Christopher's App!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    color: '#ccc',
+    fontSize: 32,
+  }
 });
